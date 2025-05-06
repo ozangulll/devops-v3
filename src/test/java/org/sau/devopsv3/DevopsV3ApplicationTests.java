22641b8bac9064d695775ae7f1dffc05d01852a1
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2025-05-06 22:43:01 +0300

    initial commit

---
+++ src/test/java/org/sau/devopsv3/DevopsV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sau.devopsv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
